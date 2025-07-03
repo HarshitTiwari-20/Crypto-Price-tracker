96c88ced9afdde71ff9716fcbbc479c8edf9af09
HarshitTiwari-20 <ht337378@gmail.com>
2025-07-04 03:37:27 +0530

    Added router && Save changes to package files and components before switching to main

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/common/Header/drawer";
+import MainComp from "../components/Dashboard/MainComp";
+import { BrowserRouter } from "react-router-dom";
+
+
+
+function DBpage() {
+    return (
+        <div className="App" >
+            hiiiii
+            fgfgfgfgfgfg
+        </div >
+    );
+}
+
+export default DBpage;
